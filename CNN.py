abef5f7422f3e58e2243e5a368dcc32b55ce8e65
Kexin (May) Ren <kexin.may@gmail.com>
2019-03-18 12:27:20 +0100

    Update and rename CNN.ipynb to CNN.py

---
+++ CNN.py
@@ -0,0 +1,184 @@
+import torch
+from torchtext import data
+from torchtext import datasets
+import random
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(tokenize='spacy')
+LABEL = data.LabelField()
+
+train_data, test_data = datasets.TREC.splits(TEXT, LABEL, fine_grained=False)
+
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+vars(train_data[-1])
+
+TEXT.build_vocab(train_data, vectors="glove.6B.100d")
+LABEL.build_vocab(train_data)
+
+print(LABEL.vocab.stoi)
+
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), 
+    batch_size=BATCH_SIZE, 
+    device=device)
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.convs = nn.ModuleList([
+                                    nn.Conv2d(in_channels = 1, out_channels = n_filters, 
+                                              kernel_size = (fs, embedding_dim)) 
+                                    for fs in filter_sizes
+                                    ])
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, text):
+        
+        #text = [sent len, batch size]
+        
+        text = text.permute(1, 0)
+                
+        #text = [batch size, sent len]
+        
+        embedded = self.embedding(text)
+                
+        #embedded = [batch size, sent len, emb dim]
+        
+        embedded = embedded.unsqueeze(1)
+        
+        #embedded = [batch size, 1, sent len, emb dim]
+        
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+            
+        #conv_n = [batch size, n_filters, sent len - filter_sizes[n]]
+        
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        
+        #pooled_n = [batch size, n_filters]
+        
+        cat = self.dropout(torch.cat(pooled, dim=1))
+
+        #cat = [batch size, n_filters * len(filter_sizes)]
+            
+        return self.fc(cat)
+
+INPUT_DIM = len(TEXT.vocab)
+EMBEDDING_DIM = 100
+N_FILTERS = 100
+FILTER_SIZES = [2,3,4]
+OUTPUT_DIM = len(LABEL.vocab)
+DROPOUT = 0.5
+
+model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
+
+pretrained_embeddings = TEXT.vocab.vectors
+
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.CrossEntropyLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+def categorical_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    max_preds = preds.argmax(dim=1, keepdim=True) # get the index of the max probability
+    correct = max_preds.squeeze(1).eq(y)
+    return correct.sum()/torch.FloatTensor([y.shape[0]])
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        
+        optimizer.zero_grad()
+        
+        predictions = model(batch.text)
+        
+        loss = criterion(predictions, batch.label)
+        
+        acc = categorical_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+
+            predictions = model(batch.text)
+            
+            loss = criterion(predictions, batch.label)
+            
+            acc = categorical_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+N_EPOCHS = 5
+
+for epoch in range(N_EPOCHS):
+
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+    
+    print(f'| Epoch: {epoch+1:02} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}% | Val. Loss: {valid_loss:.3f} | Val. Acc: {valid_acc*100:.2f}% |')
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'| Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}% |')
+
+import spacy
+nlp = spacy.load('en')
+
+def predict_class(sentence, min_len=4):
+    tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+    if len(tokenized) < min_len:
+        tokenized += ['<pad>'] * (min_len - len(tokenized))
+    indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(1)
+    preds = model(tensor)
+    max_preds = preds.argmax(dim=1)
+    return max_preds.item()
